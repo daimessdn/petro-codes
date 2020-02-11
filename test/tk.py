33511c88bdb4f97d2cac127901a58b3238a89022
daimessdn <daimessdn@gmail.com>
2020-02-11 23:35:13 +0700

    added solid relief on first label (tk.py on test)

--- test/tk.py
+++ test/tk.py
@@ -4,6 +4,6 @@ window = Tk()
 window.geometry("300x300")
 window.title("welcome")
 
-label1 = Label(window, text="welcome to Tkinter", font=("arial", 16, "bold"), fg="blue", bg="yellow").pack()
+label1 = Label(window, text="welcome to Tkinter", font=("arial", 16, "bold"), fg="blue", bg="yellow", relief="solid").pack()
 
 window.mainloop()
