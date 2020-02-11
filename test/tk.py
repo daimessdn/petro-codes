650765d7d3ca27bcbc55ef4b4a1a10135365fac9
daimessdn <daimessdn@gmail.com>
2020-02-11 23:39:11 +0700

    added expand properties for first label (tk.py on test)

--- test/tk.py
+++ test/tk.py
@@ -5,6 +5,6 @@ window.geometry("300x300")
 window.title("welcome")
 
 label1 = Label(window, text="welcome to Tkinter", font=("arial", 16, "bold"), fg="blue", bg="yellow", relief="solid")
-label1.pack(fill=BOTH, padx=2, pady=2)
+label1.pack(fill=BOTH, padx=2, pady=2, expand=True)
 
 window.mainloop()
