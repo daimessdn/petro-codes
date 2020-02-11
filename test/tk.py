3d727340479acd0853d8e89b0ee32d6af856e7cc
daimessdn <daimessdn@gmail.com>
2020-02-11 23:53:33 +0700

    added button (tk.py on test)

--- test/tk.py
+++ test/tk.py
@@ -7,4 +7,7 @@ window.title("welcome")
 label1 = Label(window, text="welcome to Tkinter", font=("arial", 16, "bold"), fg="blue", bg="yellow", relief="solid")
 label1.place(x=10, y=250)
 
+button1 = Button(window, text="DEMO", font=("arial", 12, "bold"), fg="white", bg="brown", relief="ridge")
+button1.place(x=110, y=110)
+
 window.mainloop()
