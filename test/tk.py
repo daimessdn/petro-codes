67b8b0d4c164691572dafc4dc7e5545d2e975a64
daimessdn <daimessdn@gmail.com>
2020-02-11 23:38:32 +0700

    added pading properties for first label (tk.py on test)

--- test/tk.py
+++ test/tk.py
@@ -4,6 +4,7 @@ window = Tk()
 window.geometry("300x300")
 window.title("welcome")
 
-label1 = Label(window, text="welcome to Tkinter", font=("arial", 16, "bold"), fg="blue", bg="yellow", relief="solid").pack()
+label1 = Label(window, text="welcome to Tkinter", font=("arial", 16, "bold"), fg="blue", bg="yellow", relief="solid")
+label1.pack(fill=BOTH, padx=2, pady=2)
 
 window.mainloop()
