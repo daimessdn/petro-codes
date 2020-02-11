e896e7c973b5d35b64f9204a0819b4f9bb7cf779
daimessdn <daimessdn@gmail.com>
2020-02-11 14:39:25 +0700

    create z-numeric

---
+++ reservoir/z-numeric/z.py
@@ -0,0 +1,37 @@
+# CALCULATING Z-FACTOR
+# using virial coefficient EOS of Van-der-Waals equation
+
+## initialize Python library
+import math
+
+## initialize chemical properties
+chem_name = ["CH4", "C2H6", "C3H8", "C4H10", "C5H12", "C6H14", "C7H16", "CO2", "H2S", "N2"]		# chemical name
+mol = []																						# mol percentage
+Tc = [343.33, 549.92, 666.06, 765.62, 845.8, 913.6, 972.7, 547.91, 672.45, 227.49]				# critical temperature for chemicals
+Pc = [866.4, 706.5, 615.0, 550.8, 468.6, 436.9, 396.8, 1071, 1300, 493.1]						# critical pressure for chemicals
+acc_factor = [0.0104, 0.0879, 0.1522, 0.1995, 0.2514, 0.2994, 0.3494, 0.2997, 0.0948, 0.0372]	# accentric factor
+
+## define functions
+def B0(T, Tc):
+	return (0.083 - (0.422 / math.pow(T / Tc, 1.6)))
+
+def B1(T, Tc):
+	return (0.139 - (0.172 / math.pow(T / Tc, 4.2)))
+
+def B(T, Tc, Pc, acc_factor):
+	return Tc / Pc * (B0(T, Tc) + (acc_factor * B1(T, Tc)))
+
+## receiving input
+P = float(input("masukkan nilai tekanan P (dalam psia): "))		# pressure (psia)
+T = float(input("masukkan nilai temperatur T (dalam oR): "))	# temperature (oR)
+
+for i in range(len(chem_name)):
+	mol.append(float(input("masukkan nilai persen mol utk " + chem_name[i] + ": ")))
+
+## processing
+z = 1			# init'd z-factor
+x = P / T	# init'd reciproc. volume (1/V)
+for i in range(0, len(chem_name)):
+	z += B(T, Tc[i], Pc[i], acc_factor[i]) * math.pow(x, i+1)
+
+print(z)
