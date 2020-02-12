588514ad087ed4db2f96d39ae663325583207f1b
daimessdn <daimessdn@gmail.com>
2020-02-12 16:17:36 +0700

    delete useless command (tk.py on test)

--- test/tk.py
+++ test/tk.py
@@ -10,13 +10,13 @@ def demo1():
 def demo2():
 	exit()
 
-label1 = Label(window, text="welcome to Tkinter", font=("arial", 16, "bold"), fg="blue", bg="yellow", relief="solid")
+label1 = Label(window, text="welcome to Tkinter")
 label1.pack()
 
-button1 = Button(window, text="demo", font=("arial", 12, "bold"), fg="white", bg="brown", relief="sunken", command=demo1)		# GROOVE, RIDGE, SUNKEN, RAISED, FLAT, SOLID
+button1 = Button(window, text="demo", command=demo1)		# GROOVE, RIDGE, SUNKEN, RAISED, FLAT, SOLID
 button1.pack()
 
-button2 = Button(window, text="exit", font=("arial", 12, "bold"), fg="white", bg="brown", relief="sunken", command=demo2)		# GROOVE, RIDGE, SUNKEN, RAISED, FLAT, SOLID
+button2 = Button(window, text="exit", command=demo2)		# GROOVE, RIDGE, SUNKEN, RAISED, FLAT, SOLID
 button2.pack()
 
 
