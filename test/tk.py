dc96fbeacb443b132168a8eb298be792934cb799
daimessdn <daimessdn@gmail.com>
2020-02-12 16:15:30 +0700

    testing with button (tk.py on test)

--- test/tk.py
+++ test/tk.py
@@ -4,10 +4,20 @@ window = Tk()
 window.geometry("300x300")
 window.title("welcome")
 
-label1 = Label(window, text="welcome to Tkinter", font=("arial", 16, "bold"), fg="blue", bg="yellow", relief="solid")
-label1.place(x=10, y=250)
+def demo1():
+	print("demo tkinter")
 
-button1 = Button(window, text="DEMO", font=("arial", 12, "bold"), fg="white", bg="brown", relief="ridge")
-button1.place(x=110, y=110)
+def demo2():
+	exit()
+
+label1 = Label(window, text="welcome to Tkinter", font=("arial", 16, "bold"), fg="blue", bg="yellow", relief="solid")
+label1.pack()
+
+button1 = Button(window, text="demo", font=("arial", 12, "bold"), fg="white", bg="brown", relief="sunken", command=demo1)		# GROOVE, RIDGE, SUNKEN, RAISED, FLAT, SOLID
+button1.pack()
+
+button2 = Button(window, text="exit", font=("arial", 12, "bold"), fg="white", bg="brown", relief="sunken", command=demo2)		# GROOVE, RIDGE, SUNKEN, RAISED, FLAT, SOLID
+button2.pack()
+
 
 window.mainloop()
