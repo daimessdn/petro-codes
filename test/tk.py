4f8654df2db1b95de3ff47a79a85e8d10d0cecf7
daimessdn <daimessdn@gmail.com>
2020-02-12 17:11:59 +0700

    testing with entry (tk.py on test)

--- test/tk.py
+++ test/tk.py
@@ -5,11 +5,14 @@ root = Tk()
 root.geometry("500x600")
 root.title("welcome")
 
-img = Image.open("test.jpg")
+img = Image.open("test2.jpg")
 photo = ImageTk.PhotoImage(img)
 
 lab = Label(image=photo)
 lab.pack()
+
+fn = StringVar()
+ln = StringVar()
 
 def demo1():
 	print("demo tkinter")
@@ -17,13 +20,25 @@ def demo1():
 def demo2():
 	exit()
 
-label1 = Label(root, text="welcome to Tkinter")
-label1.pack()
+label0 = Label(root, text="registration form", font=("bold", 16))
+label0.place(x=90, y=150)
 
-button1 = Button(root, text="demo", command=demo1)		# GROOVE, RIDGE, SUNKEN, RAISED, FLAT, SOLID
+label1 = Label(root, text="first name: ")
+label1.place(x=80, y=240)
+
+entry1 = Entry(root, textvar=fn)
+entry1.place(x=240, y=242)
+
+label2 = Label(root, text="last name: ")
+label2.place(x=80, y=280)
+
+entry2 = Entry(root, textvar=ln)
+entry2.place(x=240, y=282)
+
+button1 = Button(root, text="demo", command=demo1)
 button1.pack()
 
-button2 = Button(root, text="exit", command=demo2)		# GROOVE, RIDGE, SUNKEN, RAISED, FLAT, SOLID
+button2 = Button(root, text="exit", command=demo2)	
 button2.pack()
 
 
