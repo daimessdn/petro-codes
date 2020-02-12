e07efc81e71d61c39a0c7527ee5515c6d09a254a
daimessdn <daimessdn@gmail.com>
2020-02-12 16:48:47 +0700

    testing with image(tk.py on test)

--- test/tk.py
+++ test/tk.py
@@ -1,8 +1,15 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
-window = Tk()
-window.geometry("300x300")
-window.title("welcome")
+root = Tk()
+root.geometry("500x600")
+root.title("welcome")
+
+img = Image.open("test.jpg")
+photo = ImageTk.PhotoImage(img)
+
+lab = Label(image=photo)
+lab.pack()
 
 def demo1():
 	print("demo tkinter")
@@ -10,14 +17,14 @@ def demo1():
 def demo2():
 	exit()
 
-label1 = Label(window, text="welcome to Tkinter")
+label1 = Label(root, text="welcome to Tkinter")
 label1.pack()
 
-button1 = Button(window, text="demo", command=demo1)		# GROOVE, RIDGE, SUNKEN, RAISED, FLAT, SOLID
+button1 = Button(root, text="demo", command=demo1)		# GROOVE, RIDGE, SUNKEN, RAISED, FLAT, SOLID
 button1.pack()
 
-button2 = Button(window, text="exit", command=demo2)		# GROOVE, RIDGE, SUNKEN, RAISED, FLAT, SOLID
+button2 = Button(root, text="exit", command=demo2)		# GROOVE, RIDGE, SUNKEN, RAISED, FLAT, SOLID
 button2.pack()
 
 
-window.mainloop()
+root.mainloop()
