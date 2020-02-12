4c6bc22f2f2ecd7fa3a233b7ecd0142ff21e0930
daimessdn <daimessdn@gmail.com>
2020-02-12 17:24:50 +0700

    testing with droplist (tk.py on test)

--- test/tk.py
+++ test/tk.py
@@ -13,6 +13,8 @@ lab.pack()
 
 fn = StringVar()
 ln = StringVar()
+dob = StringVar()
+var = StringVar()
 
 def demo1():
 	print("demo tkinter")
@@ -25,21 +27,32 @@ label0.place(x=90, y=150)
 
 label1 = Label(root, text="first name: ")
 label1.place(x=80, y=240)
-
 entry1 = Entry(root, textvar=fn)
 entry1.place(x=240, y=242)
 
 label2 = Label(root, text="last name: ")
 label2.place(x=80, y=280)
-
 entry2 = Entry(root, textvar=ln)
 entry2.place(x=240, y=282)
+
+label3 = Label(root, text="dob: ")
+label3.place(x=80, y=320)
+entry3 = Entry(root, textvar=dob)
+entry3.place(x=240, y=322)
+
+label4 = Label(root, text="country: ")
+label4.place(x=80, y=360)
+
+list1 = ["Nepal", "India", "Canada"]
+droplist = OptionMenu(root, var, *list1)
+var.set("select country")
+droplist.config(width=15)
+droplist.place(x=240, y=360)
 
 button1 = Button(root, text="demo", command=demo1)
 button1.pack()
 
 button2 = Button(root, text="exit", command=demo2)	
 button2.pack()
-
 
 root.mainloop()
